d60b9d04ecc1c41d0369b65758ab7878bb7e697c
jmaiajoao <128436278+jmaiajoao@users.noreply.github.com>
2023-04-08 20:18:15 -0300

    Add files via upload

---
+++ atividade4.java
@@ -0,0 +1,49 @@
+
+
+// programa de soluções computacionais.
+// Lista 2 ( atividade 4)
+//Aluno: João Victor Maia Leite
+//RA: 323124931
+
+
+import java.util.Scanner;
+
+public class atividade4 {
+    public static void main(String[] args) throws Exception {
+        
+
+    // Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+    
+
+    Scanner input = new Scanner(System.in);
+    
+    int lado = input.nextInt();
+
+    System.out.println("A área do quadrado será " + lado * lado);
+
+
+    // O dobro da área será 
+
+     int resultarea = 64;
+
+    System.out.println("O dobro da área será "+ resultarea * 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
